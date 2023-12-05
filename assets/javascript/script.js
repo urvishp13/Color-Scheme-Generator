2ad207dd08d85757ebdcd5ed545d9885d072acde
Picasso <urvishp13@gmail.com>
2023-12-05 11:47:27 -0500

    made UI for clickable hex codes, added onload event to populate page with a display

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,6 +13,27 @@ let schemeChosen = selectedScheme.textContent.toLowerCase()
 const schemeColorSlots = document.getElementsByClassName("color")
 const schemeColorHexes = document.getElementsByClassName("hex")
 
+function fetchColorScheme(e) {
+    // don't wan't chosen scheme option to go back to default upon submission
+    e.preventDefault()
+    // GET the color scheme from the ColorAPI
+    // send the seed color and the scheme type to the API
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorChosen}&mode=${schemeChosen}`)
+        .then(res => res.json())
+        .then(palette => drawColorScheme(palette))
+}
+
+function drawColorScheme(palette) {
+    console.log(palette.colors)
+    const colors = palette.colors
+    // loop through all the slots, hexes, and colors
+    for (let i = 0; i < colors.length; i++) {
+        // fill corresponding slot and hex with corresponding color and hex code
+        schemeColorSlots[i].style.backgroundColor = colors[i].hex.value
+        schemeColorHexes[i].textContent = colors[i].hex.value
+    }
+}
+
 // get the color selected after closing the color wheel pop-up
 colorWheel.addEventListener("change", function(e) {
     colorChosen = e.target.value.split("#")[1]
@@ -46,21 +67,9 @@ schemeModesDropdown.addEventListener("change", function() {
 })
 
 // when all the inputs are submitted and a color scheme is wanted back
-form.addEventListener('submit', function(e) {
-    // don't wan't chosen scheme option to go back to default upon submission
-    e.preventDefault()
-    // GET the color scheme from the ColorAPI
-    // send the seed color and the scheme type to the API
-    fetch(`https://www.thecolorapi.com/scheme?hex=${colorChosen}&mode=${schemeChosen}`)
-        .then(res => res.json())
-        .then(palette => {
-            console.log(palette.colors)
-            const colors = palette.colors
-            // loop through all the slots, hexes, and colors
-            for (let i = 0; i < colors.length; i++) {
-                // fill corresponding slot and hex with corresponding color and hex code
-                schemeColorSlots[i].style.backgroundColor = colors[i].hex.value
-                schemeColorHexes[i].textContent = colors[i].hex.value
-            }
-        })
-})
+form.addEventListener('submit', fetchColorScheme)
+
+// on document load, display a placeholder scheme to make the display not seem blank and confusing
+window.addEventListener("load", fetchColorScheme)
+
+// make the hex codes clickable so can copy their value
